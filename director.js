15949c37cfe05cd9ca873f693c1b9df09648479f
Duncan Cook <duncan.cook@theletterd.co.uk>
2018-07-26 15:33:20 -0700

    directives and stuff

--- director.js
+++ director.js
@@ -7,13 +7,20 @@ async function main(document) {
 	screen = $('#screen');
 
 	for (const scene of scenes) {
+		if ("directive" in scene) {
+			if (scene.directive === "clear") {
+				$("#screen").empty();
+			}
+			continue;
+		}
+
 		console.log(scene.text);
 		scene_element = $(document.createElement('div'));
 		scene_element.append(scene.text);
 		scene_element.hide().appendTo(screen);
 
-
 		arrival = scene.arrive;
+
 		// arrival
 		if (arrival.transition == "fade") {
 		    scene_element.fadeIn(arrival.duration);
@@ -32,21 +39,54 @@ async function main(document) {
 			scene_element.show();
 		}
 
+		// animate
+		if ("animation" in scene) {
+			numFrames = scene.animation.frames.length;
+			that = scene_element;
+			animation_func = async function(frames, frame_num, that) {
+				that.empty();
+				that.append(frames[frame_num]);
+				await sleep(scene.animation.frame_length_ms);
+				next_frame = (frame_num + 1) % frames.length;
+				if (that.is(":visible")) {
+					setTimeout(animation_func, scene.animation.frame_length_ms, frames, next_frame, that);
+				}
+			};
+
+			animation_func(scene.animation.frames, 0, that);
+		}
+
 
 		// dwell
 		await sleep(scene.dwell);
 
 		// departure
 		departure = scene.depart;
-		if (departure.transition === "fade") {
-		    scene_element.fadeOut(departure.duration);
-			await sleep(departure.duration);
-	    } else if (departure.transition === "hide" ) {
-			scene_element.remove();
+		if (departure.remove === true) {
+			if (departure.transition === "hide") {
+				scene_element.remove();
+			}
+			if (departure.transition === "fade") {
+				that = scene_element;
+				scene_element.fadeOut(departure.duration, function() {
+					that.remove();
+				});
+			}
 		} else {
-			scene_element.remove();
-		}
+			if (departure.transition === "hide") {
+				scene_element.empty();
+				scene_element.append(" ");
+			} else if (departure.transition === "fade") {
+				that = scene_element
+				scene_element.fadeOut(departure.duration, function() {
+					that.show();
+					that.empty();
+				});
+			} else if (departure.transition === "keep") {
+				continue;
+			}
 
+		}
 	}
 
 }
